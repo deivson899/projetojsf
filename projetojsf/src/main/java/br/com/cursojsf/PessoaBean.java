544feba4ccef080f8e953889e3e01825c08df490
Deivson A <Deivson A@DESKTOP-IL2IPOD>
2020-09-04 19:47:48 -0300

    versão 1.o

---
+++ projetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
@@ -0,0 +1,90 @@
+package br.com.cursojsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+import com.sun.faces.taglib.html_basic.CommandButtonTag;
+
+@ManagedBean(name = "pessoaBean")
+@ViewScoped
+public class PessoaBean {
+
+	private String nome;
+	
+	private String senha;
+	
+	private String texto;
+	
+	private HtmlCommandButton commandButton;
+	
+	
+	private List<String> nomes = new ArrayList<String>();
+	
+	
+
+	public String addNome() {
+		nomes.add(nome);
+		
+		if(nomes.size() > 3) {
+			commandButton.setDisabled(true);
+			return "paginanavegada?faces-redirect=true";
+		}
+		return "";
+	}
+
+	
+	
+	public String getSenha() {
+		return senha;
+	}
+
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+
+	public String getTexto() {
+		return texto;
+	}
+
+
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+
+
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+	
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+	
+
+}
